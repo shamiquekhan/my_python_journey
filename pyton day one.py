243eb6c1c717da58c461f183b6960ab4f2d09fef
Daily Decoder <122252741+shamiquekhan@users.noreply.github.com>
2024-07-17 05:44:02 +0530

    Add files via upload

---
+++ pyton day one.py
@@ -0,0 +1,48 @@
+# day 1 of coding
+name = input("enter your name : ")
+print("Welcome", name , "!!!")
+print(type(name))
+print(name)
+salary = 500000
+assets= 100000
+Libility =20000
+sum= salary+assets-Libility
+print(sum)
+# ctrl + / = interchange code to comment
+# arithmatic operators
+a= 10 
+b= 2 
+print(a*b)
+print(a/b)
+print(a%b) # for re minder
+print(a//b) # for only int , no decimal value
+print(a**b)
+ #relational operators
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<b)
+# assignment Operators
+# x=int(input("enter a number : "))
+x = 10
+x=x+10
+x+=10 # both do same function
+x*=10
+print(x)
+# logical Operators
+print(not True) # case sensetive , give opposite vale
+print(not(a<b))
+# and & Or
+val1=True
+val2=False
+print(val1 and val2)
+print(val1 or val2)
+print((a==b) and val2)
+# type converstion(atomatic) and casting(manual)
+c=2.5
+sum2=b+c
+print(sum2)
+d="2"
+e=int(d) #casting
+print(type(e))
+print(a+e)
